3fb3f3b1e8aeba01dd2129026b041a71308ace75
Abhinav Dhakal <128499381+abhidhakal@users.noreply.github.com>
2025-06-13 21:53:22 +0545

    added folders

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hready/view/splash_screen.dart';
-import 'package:hready/view/theme/hready_theme.dart';
+import 'package:hready/app/theme/hready_theme.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
